e846497c4d68334a626ed72da38238a7e900fe59
UzuKenx <agentmaxrock@gmail.com>
2022-10-03 13:46:38 -0400

    Initialized Project

---
+++ src/main/java/com/example/CS3141R01Team2/Cs3141R01Team2Application.java
@@ -0,0 +1,13 @@
+package com.example.CS3141R01Team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs3141R01Team2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs3141R01Team2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CS3141R01Team2/Cs3141R01Team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CS3141R01Team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs3141R01Team2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
