db6a21cd23421925b658da16ef464d4dc5d1f77f
hanbyian <hanbyian325@gmail.com>
2022-10-12 23:14:31 -0400

    added frontend components and functions to App.js

--- frontend/studyup/src/App.js
+++ frontend/studyup/src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom/client';
+import React, { Component } from 'react';
 
 function App() {
+  function SignIn(){
+    function confirmUser(){//TODO: Hunter to api call
+      //props.password and props.username using props param instead?
+      //this function calls api to confirm username and password signin, if signin is a good account then render home page.
+      const data = {};//data is what we get from api call
+      root.render(main({name:"Ian Hanby"}));
+      return;
+    }
+    return(
+      <div className="signInForm">
+        <form onSubmit={confirmUser}>
+          <input type="text" placeholder="username"></input>
+          <input type="text" placeholder="password"></input>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    );
+  }
+  class User extends React.Component {
+    
+    render(){
+      return <h3>Hello, {this.props.name}</h3>//use: <User name="Ian Hanby"/>
+    }
+  }
+ 
+  const header = (
+    <div className="header">
+      <h1>StudyUp</h1>
+      <h2>Sponsored by Blizzard T. Husky</h2>
+    </div>
+  );
+
+  //navBar div just has sample stuff for now, need to finish later
+  const navBar = (
+    <div className="navBar">
+      
+      <button>My Sets</button>
+      <button>Create New Set</button>
+    </div>
+  );
+  const main =(props)=>(
+    <div>
+      {header}
+      <User name={props.name}/>
+      {navBar}
+    </div>
+  );
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container);
+  const signInPage = <SignIn/>;
+  root.render(signInPage);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="root">
+
+      </div>
     </div>
   );
 }
