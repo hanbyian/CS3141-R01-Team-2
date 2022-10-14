5a72db8b89d42edd16073330732dc960504d56f0
Mia Kelly <miayn24@gmail.com>
2022-10-14 13:51:54 -0400

    Updated dependencies to package as .war

--- backend/src/main/java/com/example/CS3141R01Team2/Cs3141R01Team2Application.java
+++ backend/src/main/java/com/example/CS3141R01Team2/Cs3141R01Team2Application.java
@@ -2,9 +2,16 @@ package com.example.CS3141R01Team2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs3141R01Team2Application {
+public class Cs3141R01Team2Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Cs3141R01Team2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs3141R01Team2Application.class, args);
