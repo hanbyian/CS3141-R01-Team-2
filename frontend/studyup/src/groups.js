2c2fe1898e559c4a85361a84a3c93d86c62ec746
hanbyian <hanbyian325@gmail.com>
2022-10-18 11:13:01 -0400

    added create set functionality

--- frontend/studyup/src/groups.js
+++ frontend/studyup/src/groups.js
@@ -20,7 +20,7 @@ export class LoginPage extends React.Component{
         this.setState({showLogin:(!this.state.showLogin)});
     }
     confirmUser(){
-        root.render(<HomePage setKey={Object.keys(sampleData.sets)[0]} setValue={Object.entries(sampleData.sets)[0][1]} sets={sampleData.sets}/>);
+        root.render(<HomePage setKey={Object.entries(sampleData.sets)[0][0]} setValue={Object.entries(sampleData.sets)[0][1]} sets={sampleData.sets}/>);
     }
     registerUser(){
 
@@ -60,9 +60,11 @@ export class LoginPage extends React.Component{
 export class HomePage extends React.Component{
     constructor(props){
         super(props);
-        this.state = {viewingSetKey:this.props.setKey, viewingSetValue:this.props.setValue, allSets:this.props.sets, isCreating:false}
+        this.state = {viewingSetKey:this.props.setKey, viewingSetValue:this.props.setValue, allSets:this.props.sets, isCreating:false, newSet:[], newSetCount:0};
         this.handleCreating = this.handleCreating.bind(this);
         this.handleViewingSet = this.handleViewingSet.bind(this);
+        this.addTerm = this.addTerm.bind(this);
+        this.pushSet=this.pushSet.bind(this);
     }
     handleCreating(){
         this.setState({isCreating:true});
@@ -71,12 +73,48 @@ export class HomePage extends React.Component{
         this.setState({viewingSetKey:newViewingKey, viewingSetValue:newViewingValue, isCreating:false});
         
     }
+    addTerm(){
+        let nextTerm = (
+            <div className="set element" id={this.state.newSet.length}>
+                <input placeholder="term" id={this.state.newSet.length+"term"}></input>
+                <input placeholder="definition" id={this.state.newSet.length+"definition"}></input>
+            </div>
+        );
+        this.setState({newSetCount:this.state.newSet.push(nextTerm)});
+    }
+    pushSet(){
+        let currentSet={};
+        for(var i = 0;i<(this.state.newSetCount);i++){
+            let term = document.getElementById(i+"term").value;
+            let definition = document.getElementById(i+"definition").value;
+            console.log(term);
+            console.log(definition);
+            currentSet[term]=definition.toString();
+        }
+        let setName = document.getElementById("newSetName").value.toString();
+        let emptySet = {};emptySet[setName.toString()]=currentSet;
+        //this.setState({allSets:emptySet});
+        this.state.allSets[setName]=currentSet;
+        this.setState({newSetCount:this.state.newSetCount});
+    }
     render() {
         let setView;
         if(this.state.isCreating){
+            let firstTerm = (
+            <div className="set element" id={this.state.newSet.length} >
+                <input placeholder="term" id={this.state.newSet.length+"term"}></input>
+                <input placeholder="definition" id={this.state.newSet.length+"definition"}></input>
+            </div>
+            );
+            if(this.state.newSet.length==0)this.state.newSet.push(firstTerm)
             setView=(
-            <div>
-                <p>Create Set here</p>
+            <div className="newSet">
+                <input type="text" placeholder="Name of Study Set" id="newSetName"></input>
+                <div className="memberList" id="createSet">
+                    {this.state.newSet}
+                </div>
+                <button onClick={this.addTerm}>+ Add new term</button>
+                <button onClick={this.pushSet}>Finish Making Set</button>
             </div>);
         }else{
             //setView for the currently selected set(stored in state) which shows the terms and has study button
@@ -87,7 +125,6 @@ export class HomePage extends React.Component{
                 <button>Study This Set</button>
             </div>);
         }
-        //returns header, then either creating view or set view then all sets box
         return (
             <div>
             <div className="header">
@@ -98,7 +135,7 @@ export class HomePage extends React.Component{
             <div>
                 <h3>My Sets</h3>
                 <ul>
-                    {Object.entries(this.state.allSets)==0? <p>you have no sets</p>: Object.entries(this.state.allSets).map(([key,value]) => (<li><button onClick = {(e) => this.handleViewingSet(key,value,e)}>{key}</button></li>))}
+                    {this.state.allSets.length==0? <p>you have no sets</p>: Object.entries(this.state.allSets).map(([key,value]) => (<li><button onClick = {(e) => this.handleViewingSet(key,value,e)}>{key}</button></li>))}
                 </ul>
                 <button onClick={this.handleCreating}>+ Create Set</button>
                 <p>Select a set to view it and study</p>
