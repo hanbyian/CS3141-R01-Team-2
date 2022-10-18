65395312bacbee1f195319afc6238345e07eec37
Wmisip <90335755+Wmisip@users.noreply.github.com>
2022-10-18 12:00:24 -0400

    databaseDevelopment v0.5
    Added some business logic

--- backend/src/main/java/com/example/CS3141R01Team2/Users/UsersService.java
+++ backend/src/main/java/com/example/CS3141R01Team2/Users/UsersService.java
@@ -1,6 +1,7 @@
 package com.example.CS3141R01Team2.Users;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
@@ -29,7 +30,19 @@ public class UsersService {
      * @param name
      */
     public void createUser(String username, String password, String email, String name) {
-        usersRepository.save(new Users(username, password, email, name));
+        Optional<Users> getUserByUsername = usersRepository.findByUsername(username);
+
+        if(getUserByUsername.isPresent()){
+            throw new IllegalArgumentException("username already exists!");
+        } else{
+            Optional<Users> getUserByEmail = usersRepository.findByEmail(email);
+            if(getUserByEmail.isPresent()){
+                throw new IllegalArgumentException("email already taken!");
+            } else{
+                usersRepository.save(new Users(username, password, email, name));
+            }
+        }
+
     }
 
 //    public boolean confirmUser(String username, String password) {
