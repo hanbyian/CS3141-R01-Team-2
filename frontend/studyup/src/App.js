f666b0a320c40b7728441eee2d9b2db44d2a5557
hanbyian <hanbyian325@gmail.com>
2022-10-27 18:21:51 -0400

    added api tests to use when I deploy war

--- frontend/studyup/src/App.js
+++ frontend/studyup/src/App.js
@@ -1,9 +1,52 @@
 import './App.css';
 import {React, Component} from 'react';
 import { LoginPage,HomePage } from './groups.js';
-
+function testAPI(){
+  try{
+    console.log(fetch("http://54.146.234.48:8080/StudyUp2/studyset/showSets"));
+  }
+  catch(e){
+    console.log(e);
+    console.log("test 1 did not work");
+  }
+  try{
+    console.log(fetch("http://54.146.234.48:3000/StudyUp2/studyset/showSets"));
+  }
+  catch(e){
+    console.log(e);
+    console.log("test 2 did not work");
+  }
+  try{
+    console.log(fetch("http://54.146.234.48:8080/StudyUp2/users/showusers"));
+  }
+  catch(e){
+    console.log(e);
+    console.log("test 3 did not work");
+  }
+  try{
+    console.log(fetch("http://54.146.234.48:3000/StudyUp2/users/showusers"));
+  }
+  catch(e){
+    console.log(e);
+    console.log("test 4 did not work");
+  }
+  try{
+    console.log(fetch("http://54.146.234.48:8080/StudyUp2/studyset/showSetsForUser/wmisip"));
+  }
+  catch(e){
+    console.log(e);
+    console.log("test 5 did not work");
+  }
+  try{
+    console.log(fetch("http://54.146.234.48:3000/StudyUp2/studyset/showSetsForUser/wmisip"));
+  }
+  catch(e){
+    console.log(e);
+    console.log("test 5 did not work");
+  }
+}
 function App(){
-  
+    testAPI();
     return (
       <div>
         <LoginPage/>
