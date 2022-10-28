22be64a34c4b318d2f42608f88a82cd87864822e
Ian Hanby <ianhanby@rover-234-176.rovernet.mtu.edu>
2022-10-28 07:51:27 -0400

    added test get requests and test fetch request

--- frontend/studyup/src/App.js
+++ frontend/studyup/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {React, Component} from 'react';
 import { LoginPage,HomePage } from './groups.js';
+let testedPost = false;
 function testAPI(){
   try{
     console.log(fetch("http://54.146.234.48:8080/StudyUp2/studyset/showSets"));
@@ -10,39 +11,26 @@ function testAPI(){
     console.log("test 1 did not work");
   }
   try{
-    console.log(fetch("http://54.146.234.48:3000/StudyUp2/studyset/showSets"));
+    console.log(fetch("http://54.146.234.48:8080/StudyUp2/users/showusers"));
   }
   catch(e){
     console.log(e);
     console.log("test 2 did not work");
   }
   try{
-    console.log(fetch("http://54.146.234.48:8080/StudyUp2/users/showusers"));
+    let data = {setName:"mySet", setOwner:"ian hanby"};
+
+    fetch("http://54.146.234.48:8080/StudyUp2/studyset/createStudySet", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'}, 
+      body: JSON.stringify(data)
+    }).then(res => {
+      console.log("Request complete! response:", res);
+    });
   }
   catch(e){
     console.log(e);
     console.log("test 3 did not work");
-  }
-  try{
-    console.log(fetch("http://54.146.234.48:3000/StudyUp2/users/showusers"));
-  }
-  catch(e){
-    console.log(e);
-    console.log("test 4 did not work");
-  }
-  try{
-    console.log(fetch("http://54.146.234.48:8080/StudyUp2/studyset/showSetsForUser/wmisip"));
-  }
-  catch(e){
-    console.log(e);
-    console.log("test 5 did not work");
-  }
-  try{
-    console.log(fetch("http://54.146.234.48:3000/StudyUp2/studyset/showSetsForUser/wmisip"));
-  }
-  catch(e){
-    console.log(e);
-    console.log("test 5 did not work");
   }
 }
 function App(){
